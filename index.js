c0a6d0fa6f5c69879c17366313d78cbcc1db0f60
Madina <mukhammetalimadina@gmail.com>
2024-01-23 19:31:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+// alert / confirm / prompt.
+// 'use strict'
+// const user = {
+//     name: prompt('What is your name?', ''),
+//     age: prompt('how old are you?', ''),
+//     gender: prompt('your gender(w/m)', '')
+// };
+//  console.log('my name is ', user.name);
+//  console.log('i am ', user.age);
+//  console.log('i am ', user.gender);
+
+
+//Работа с операторами.
+// let userInput = prompt('how old are you?', '');
+// let output = (Number(userInput));
+
+// if(!isNaN(output)) {
+//     alert("It is number: " + output);
+// }
+// else{
+//     alert('It is not number!')
+// }
+
+
+// changed for to while
+// let i = 0;
+// while(i < 3){
+//     i++;
+//     alert(`number ${i}!`);
+// }
+
+
+//Посчитайте сумму всех четных чисел в промежутке 1-100. 
+//Выведите в консоль.
+// for(let i = 1; i <= 100; i++){
+//     if(i % 2 ==0){
+//         console.log(i)
+//     }
+// }
+
+
+// Перепишите этот блок кода с использованием оператора switch.
+// let a = +prompt('a?', '');
+// console.log(a) // string
+// a = +a
+// console.log(a) // number
+
+// switch (a){
+//     case 0:
+//         alert( 0 );
+//         break;
+//     case 1:
+//         alert( 1 );
+//         break;
+//     case (2 || 3 ):
+//         alert( '2,3');
+//         break;
+// }
+
+
+
+
